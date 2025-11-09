b615f666264fd34f52d3fb25c124eff43ae71858
Jean-Marc MONTOUT <mjean_marc@hotmail.com>
2025-11-09 09:32:11 +0000

    Refactor le composant Habit pour améliorer la gestion des indicateurs partiels et ajuster les styles du composant. Ajout de la classe main-content et mise à jour de la structure HTML.

--- habbit-tracker/src/Habit.jsx
+++ habbit-tracker/src/Habit.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { FaCirclePlus, FaSquareCheck } from "react-icons/fa6";
 
-export default function Habit({ text, status, type, ...props }) {
+export default function Habit({ text, status, value, ...props }) {
     console.log(props);
+
+    const partialIndicatorsElements = [];
+    for (let i = 0; i < value; i++) {
+        partialIndicatorsElements.push(<li key={i}><FaSquareCheck /></li>);
+    }
+
     return (
-        <div className={`habit ${status}`}>
-            <div>{text}</div>
-            { (status === 'in-progress') ? (
-            <button className="btn">
-                <FaCirclePlus />
-            </button>
-            ) : (
-            <div className="indicator">
-                <FaSquareCheck />
+        <div className={`habit ${status} ${value}`}>
+
+            <div className="main-content">
+                <div>{text}</div>
+
+                {(status === 'done') ? (
+                    <div className="indicator">
+                        <FaSquareCheck />
+                    </div>
+                ) : (
+
+                    <button className="btn">
+                        <FaCirclePlus />
+                    </button>
+                )}
             </div>
-            ) }
+
+            {value > 1 && (
+                <ul className="partial-indicators">
+                    {partialIndicatorsElements}
+                </ul>
+            )}
 
 
         </div>
